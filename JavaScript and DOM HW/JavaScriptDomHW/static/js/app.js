12a57f692a0779633e5890d4d7a70b0e8e972e0e
pjohsz <patricia.johsz@experian.com>
2018-11-05 17:45:53 -0800

    corrected code, stupid variable name!

--- JavaScript and DOM HW/JavaScriptDomHW/static/js/app.js
+++ JavaScript and DOM HW/JavaScriptDomHW/static/js/app.js
@@ -1,25 +1,27 @@
-// get data from data.js input file.  
+// get data from data.js input file.
 var tableData = data;
-  console.log("accepting the data file");
-  console.log(tableData);
+// console.log("accepting the data file");
+// console.log(tableData);
 
 // Get references to the tbody element and the date of the input
 var tbody = document.querySelector("tbody");
+var dateInput = document.getElementById("datetime");
 
-// Event listener for updates on to the searchButton.  
-document.getElementById("filter-btn").addEventListener("click", handleSearchButtonClick);
+// Event listener for updates on to the searchButton.
+document
+  .getElementById("filter-btn")
+  .addEventListener("click", handleSearchButtonClick, false);
 
 // Initial render of the table (all data) renderTable creates the tableData into the tbody
-function renderTable() {
+function renderTable(dtaSet) {
   tbody.innerHTML = "";
-  console.log("Initial Render of the table")
+  // console.log("Initial Render of the table");
 
   // Load the table from input file
 
-  for (var i = 0; i < tableData.length; i++) {
-    
+  for (var i = 0; i < dtaSet.length; i++) {
     // Get get the current sighting object and its fields
-    var sighting = tableData[i];
+    var sighting = dtaSet[i];
     var fields = Object.keys(sighting);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var row = tbody.insertRow(i);
@@ -32,32 +34,24 @@ function renderTable() {
   }
 }
 
-function handleSearchButtonClick() {
-  // Get rid of the leading and trailing whitespace in the input field so we can use the date in the table search. 
+function handleSearchButtonClick(e) {
+  e.preventDefault();
+  // Get rid of the leading and trailing whitespace in the input field so we can use the date in the table search.
   var filterDate = dateInput.value.trim();
-    console.log("Search button clicked");
-    console.log(filterDate)
-  
-  if (filterDate.length != 0){
-    filteredUFO = dataSet.filter(function(sighting) {
+  // console.log("Search button clicked");
+  // console.log(filterDate);
+
+  if (filterDate.length != 0) {
+    filteredUFO = tableData.filter(function(sighting) {
       var sightingDate = sighting.datetime;
-       return sightingDate === filterDate;
-         });
+      return sightingDate === filterDate;
+    });
+  } else {
+    fileredUFO = dataSet;
   }
-  else {
-    fileredUFO = dataSet
-  }
-  if (filterState.length != 0){
-    filteredUFO = filteredUFO.filter(function(sighting) {
-      var sightingState = sighting.state;
-       return sightingState === filterState;
-         });
-  }
-  else {
-    filteredUFO = filteredUFO
-  }
-  renderTable();
+
+  renderTable(filteredUFO);
 }
 
 // Render the table for the first time on page load
-renderTable();
+renderTable(tableData);
